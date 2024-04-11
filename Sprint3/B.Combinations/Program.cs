9869af1387aa59ceae21084c2907bdb7ada95551
Pavel Egorov <egorov.p@gmail.com>
2024-04-11 19:18:35 +0300

    Sprint3 begin

---
+++ Sprint3/B.Combinations/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class B
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    private static Dictionary<char, string> _phoneKeysMap = new Dictionary<char, string>()
+    {
+        {'2',"abc"},
+        {'3',"def"},
+        {'4',"ghi"},
+        {'5',"jkl"},
+        {'6',"mno"},
+        {'7',"pqrs"},
+        {'8',"tuv"},
+        {'9',"wxyz"}
+    };
+
+    private static void GenerateCombinations(string inputKeys)
+    {
+        //var res = Combine(inputKeys, String.Empty);
+
+        //writer.WriteLine(res);
+
+        Combine2(inputKeys, String.Empty);
+    }
+
+    private static void Combine2(string input, string prefix)
+    {
+        if (String.IsNullOrEmpty(input))
+        {
+            writer.Write(prefix+" ");
+        }
+        else
+        {
+            foreach (var symbol in _phoneKeysMap[input[0]])
+            {
+                prefix += symbol;
+
+                Combine2(input.Substring(1), prefix);
+
+                prefix = prefix[..^1];
+            }
+        }
+
+    }
+
+
+    private static string Combine(string input, string path)
+    {
+        string result = String.Empty;
+
+        if (String.IsNullOrEmpty(input))
+        {
+            //writer.WriteLine(path);
+            path += " ";
+
+            return path;
+        }
+
+        foreach (var key in _phoneKeysMap[input[0]])
+        {
+            path += key;
+
+            result += Combine(input.Substring(1), path);
+            
+            path = path.Substring(0, path.Length - 1);
+        }
+        
+
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var list = reader.ReadLine();
+
+        GenerateCombinations(list);
+
+        reader.Close();
+        writer.Close();
+    }
+
+}
---
+++ Sprint3/L.TwoBicycles/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class L
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var n = ReadInt();
+
+        var list = ReadList();
+
+        var price = ReadInt();
+
+        var days = CalculateDayToPrice(n, list, price);
+
+
+        foreach (var day in days)
+        {
+            writer.Write($"{day} ");
+        }
+
+        reader.Close();
+        writer.Close();
+    }
+
+    private static List<int> CalculateDayToPrice(int n, List<int> list, int price)
+    {
+        var result = new List<int>();
+
+        result.Add(BinSearch(list.ToArray(), price, left: 0, right: list.Count));
+
+        result.Add(BinSearch(list.ToArray(), price*2, left: 0, right: list.Count));
+
+        return result;
+    }
+
+    public static int BinSearch(int[] arr, int x, int left, int right)
+    {
+        if (right <= left) // промежуток пуст
+        {
+            return -1;
+        }
+
+        // промежуток не пуст
+        int mid = (left + right) / 2;
+
+        if (mid == 0 ||
+            (arr[mid] >= x && 
+            arr[mid-1] < x)) // центральный элемент — искомый
+        {
+            return mid + 1;
+        }
+        else if (x <= arr[mid]) 
+        {
+            return BinSearch(arr, x, left, mid);
+        }
+        else 
+        {
+            return BinSearch(arr, x, mid + 1, right);
+        }
+    }
+
+    private static List<int> ReadList()
+    {
+        return reader.ReadLine()
+            .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+    }
+
+    private static int ReadInt()
+    {
+        return int.Parse(reader.ReadLine());
+    }
+}
