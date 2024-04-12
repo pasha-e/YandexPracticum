87d89ec78b485668e0d69985156fb6f48d6ee48b
Pavel Egorov <egorov.p@gmail.com>
2024-04-12 18:27:19 +0300

    add  tasks H , J

---
+++ Sprint3/H.BigNumber/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class H
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var n = ReadInt();
+
+        var list = ReadList();
+
+        InsertionSortByComparator(list, Compare);
+
+        reader.Close();
+        writer.Close();
+    }
+
+    static void InsertionSortByComparator(List<string> array, Func<string, string, bool> greater)
+    {
+        for (int i = 1; i < array.Count; i++)
+        {
+            var itemToInsert = array[i];
+
+            int j = i;
+
+            while (j > 0 && greater(itemToInsert, array[j - 1]))
+            {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = itemToInsert;
+        }
+
+        foreach (var item in array)
+        {
+            writer.Write(item);
+        }
+    }
+
+    // функция-компаратор
+    static bool Compare(string first, string second)
+    {
+        int firstInt = Convert.ToInt32(first + second);
+        int secondInt = Convert.ToInt32(second + first);
+
+        if (firstInt > secondInt)
+            return true;
+
+        return false;
+    }
+
+    private static List<string> ReadList()
+    {
+        return reader.ReadLine()
+            .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries).ToList();
+    }
+
+    private static int ReadInt()
+    {
+        return int.Parse(reader.ReadLine());
+    }
+}
---
+++ Sprint3/J.BubbleSort/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class J
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var n = ReadInt();
+
+        var list = ReadList();
+
+        BubbleSort(n, list);
+
+        reader.Close();
+        writer.Close();
+    }
+
+    private static void BubbleSort(int n, List<int> list)
+    {
+        for (int j = 0; j < n-1; j++)
+        {
+            bool needSort = false;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (list[i] > list[i + 1])
+                {
+                    (list[i], list[i + 1]) = (list[i + 1], list[i]);
+                    needSort = true;
+                }
+            }
+
+            if (!needSort && j != 0)
+                break;
+
+            PrintList(list);
+        }
+    }
+
+    private static void PrintList(List<int> list)
+    {
+        foreach (var elem in list)
+            writer.Write(elem + " ");
+
+        writer.WriteLine();
+    }
+
+
+    private static List<int> ReadList()
+    {
+        return reader.ReadLine()
+            .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+    }
+
+    private static int ReadInt()
+    {
+        return int.Parse(reader.ReadLine());
+    }
+}
