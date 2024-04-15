d117fda8fbb2a2b91dfc8369e266db3a661e36e2
Pavel Egorov <egorov.p@gmail.com>
2024-04-15 18:14:09 +0300

    final tasks for  sprint 3

---
+++ Sprint3/Final/Final1.BrokenArraySearch/Program.cs
@@ -0,0 +1,190 @@
+﻿/*
+https://contest.yandex.ru/contest/23815/run-report/112123270/
+ */
+
+/*
+   -- ПРИНЦИП РАБОТЫ --
+
+    По условию, входные данных состоят из двух отсосртированных подмассиво.
+    Необходимо найти точку (индекс) на котором случился "перелом". 
+    Эта точка и будет нашей опорной точкой, разделяющей массив на два отсортированных подмассива
+    Так как подмассивы отсортированы мы можем определить в какой из них необходимо искать элемент
+    Выполнить бинарный поиск по подмассиву, с искомым элементом
+    
+   -- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+
+    в работе алгоритма будем использовать прицнипы бинарного поиска
+    Бинарный поиск заботает по принципу разделяй и властвуй. 
+    Т.е разделяем задачу на подзадачи и решаем их отдельно. Посторяя этот механизм рекурсивно.
+    Такой подход позволяет осуществлять эффективный поиск по упорядоченным наборам данных.
+    
+   -- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+
+   Поиск опорной точки в массиве из n имеет временную сложность О(lon n), так как поиск идёт методом деления пополам
+   Бинарный поиск в подмассивах так же имеет пространственную сложность О(lon n)
+   Соответственно общая временная сложность  О(lon n)
+
+   -- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+
+    Поиск опорной точки имеет пространственную сложность О(1)
+    Бинарный поиск в подмассивах так же имеет пространственную сложность О(1)
+    Соответственно общая сложность также О(1)
+
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Solution
+{
+    public static int BrokenSearch(List<int> array, int el)
+    {
+        int pivot = FindPivot(array);
+
+        if (array[pivot] == el)
+            return pivot;
+
+        //искомый элемент больше первого элемента массива - значит ищем в первой части
+        if (array[0] <= el)
+            return BinarySearch(array, el, 0, pivot);
+        
+        // иначе во второй
+        return BinarySearch(array, el, pivot+1, array.Count);
+    }
+
+    private static int BinarySearch(List<int> array, int el, int left, int right)
+    {
+        if (right <= left) //границы сместились, промежуток пуст
+        {
+            return -1;
+        }
+
+        // промежуток не пуст
+        int mid = (left + right) / 2;
+
+        if (array[mid] == el) // центральный элемент — искомый
+        {
+            return mid;
+        }
+        else if (el < array[mid]) // искомый элемент меньше центрального значит следует искать в левой половине
+        {
+            return BinarySearch(array, el, left, mid);
+        }
+        else // иначе следует искать в правой половине
+        {
+            return BinarySearch(array, el, mid + 1, right);
+        }
+    }
+    
+
+    /// <summary>
+    /// Поиск опорной точки.  У нас это будет точка "перелома"
+    /// </summary>
+    /// <param name="list"></param>
+    /// <returns>индекс опорной точки</returns>
+    private static int FindPivot(List<int> list)
+    {
+        var left = 0;
+        var right = list.Count -1;
+
+        while (left <= right)
+        {
+            var mid = (left + right) / 2;
+
+            if (list[mid] >= list[right])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        if (right > 0)
+            return right;
+
+        return list.Count - 1;
+    }
+   
+#if !REMOTE_JUDGE
+    private static void Main()
+    {
+        var arr = new List<int> { 19, 21, 100, 101, 1, 4, 5, 7, 12 };
+        Console.WriteLine(BrokenSearch(arr, 5) == 6);
+
+        var arr2 = new List<int> { 19, 21, 100, 101, 102, 103, 1, 4, 5, 7, 12, 13 };
+        Console.WriteLine(BrokenSearch(arr2, 5) == 8);
+
+        var arr3 = new List<int> { 19, 21, 100, 101, 102, 103, 1, 4, 5, 7, 12, 13 };
+        Console.WriteLine(BrokenSearch(arr3, 13) == 11);
+
+        var arr4 = new List<int> { 19, 21, 100, 101, 102, 103, 1, 4, 5, 7, 12, 13 };
+        Console.WriteLine(BrokenSearch(arr4, 19) == 0);
+
+        var arr5 = new List<int> { 19, 21, 100, 101, 102, 103, 1, 4, 5, 7, 12, 13 };
+        Console.WriteLine(BrokenSearch(arr5, 105) == -1);
+
+        var arr6 = new List<int> { 19, 21, 100, 101, 102, 103, 1, 4, 5, 7, 12, 13 };
+        Console.WriteLine(BrokenSearch(arr6, 1) == 6);
+
+        var arr7 = new List<int> { 1, 5 };
+        Console.WriteLine(BrokenSearch(arr7, 1) == 0);
+
+        var arr10 = new List<int> { 1, 5 };
+        Console.WriteLine(BrokenSearch(arr10, 5) == 1);
+        
+        var arr8 = new List<int> { 5, 1 };
+        Console.WriteLine(BrokenSearch(arr8, 1) == 1);
+
+        var arr9 = new List<int> { 3, 5, 6, 7, 9, 1, 2 };
+        Console.WriteLine(BrokenSearch(arr9, 4) == -1);
+
+    }
+#endif
+
+}
+
+/*
+9
+5
+19 21 100 101 102 103 1 4 5 7 12 13
+8
+
+9
+13
+19 21 100 101 102 103 1 4 5 7 12 13
+11
+
+9
+19
+19 21 100 101 102 103 1 4 5 7 12 13
+0
+
+9
+105
+19 21 100 101 102 103 1 4 5 7 12 13
+-1
+
+9
+1
+19 21 100 101 102 103 1 4 5 7 12 13
+6
+
+
+2
+1
+1 5
+0
+
+2
+1
+5 1
+1
+
+7
+4
+3 5 6 7 9 1 2
+-1
+ */
---
+++ Sprint3/Final/Final2.EffectiveQuickSort/Program.cs
@@ -0,0 +1,199 @@
+﻿/*
+ https://contest.yandex.ru/contest/23815/run-report/112098543/
+ */
+
+/*
+   -- ПРИНЦИП РАБОТЫ --
+   (из описания задачи)
+
+    Как и в случае обычной быстрой сортировки, которая использует дополнительную память, 
+    необходимо выбрать опорный элемент (англ. pivot), а затем переупорядочить массив. 
+    Сделаем так, чтобы сначала шли элементы, не превосходящие опорного, а затем —– большие опорного.
+   
+    Затем сортировка вызывается рекурсивно для двух полученных частей. 
+    Именно на этапе разделения элементов на группы в обычном алгоритме используется дополнительная память. 
+
+    Пусть мы как-то выбрали опорный элемент. 
+    Заведём два указателя left и right, которые изначально будут указывать на левый и правый концы отрезка соответственно. 
+    Затем будем двигать левый указатель вправо до тех пор, пока он указывает на элемент, меньший опорного. 
+    Аналогично двигаем правый указатель влево, пока он стоит на элементе, превосходящем опорный. 
+    В итоге окажется, что что левее от left все элементы точно принадлежат первой группе, а правее от right — второй. 
+    Элементы, на которых стоят указатели, нарушают порядок. 
+    Поменяем их местами и продвинем указатели на следующие элементы. 
+    Будем повторять это действие до тех пор, пока left и right не столкнутся.
+
+   
+   -- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+
+    Быстрая сортировка основана на принципе разделяй и властвуй. 
+    Делим входные данные на части относительно опорной точки
+    Случайный выбор опорной точки  даёт нам шанс надётся что мы не попадём на худший случай временой сложности.
+    Элементы меньшие опорной точки сдвигаем влево, большие вправо.
+    Рекурсивно повторяем до достижении цели.
+
+    Сравнение элементов осуществляется на выбраннах нами принциципах сравнения.
+   
+   
+   -- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+   
+    Временная сложность алгоритма быстрйо сортировки зависит от глубины рекурсии.
+    Которая в свою очередь зависит от колисечтва элементов n и успешности выбора опорной точки.
+    Значит общая сложность определяется количеством разбиений на подмассивы (глубиной рекурсии)
+     Худший случай О(n^2)  - когда один из подмассивов будет всегда пуст
+     Лучший случай О(n*log n) - когда разделение будет такого что подмассивы будут одинаковы
+     Поосколько  худший случай это особо "подогнанный" вариант входных данных и выбора опорной точки,
+     можно сказать что в среднем сложность будет О(n*log n)
+
+   
+   -- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+   
+   Алгоритм не задействует внешней памяти зависимой от размера входных данных. Поэтому его сложность можно определить как О(1)
+    
+   
+
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Final2QuickSort
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    private static void EffectiveQuickSort(List<UserInfo> userInfoList)
+    {
+        QuickSort(ref userInfoList, inLeft: 0, inRight: userInfoList.Count -1);
+
+        foreach (var userInfo in userInfoList)
+        {
+            writer.WriteLine(userInfo.ToString());
+        }
+    }
+
+    /// <summary>
+    /// рекурсивная функция быстрой сортировки на месте.
+    /// </summary>
+    /// <param name="list">ссылка на список (передаём по ссылке чтобы изменять входные данные, бз создания копии)</param>
+    /// <param name="inLeft">указатель (индекс) левой границы </param>
+    /// <param name="inRight">указатель (индекс правой границы)</param>
+    private static void QuickSort(ref List<UserInfo> list, int inLeft, int inRight)
+    {
+        if (inLeft >= inRight)
+        {
+            return ;
+        }
+
+        // опорная точка
+        var pivot = list[FindPivot(inLeft, inRight)];
+
+        var left = inLeft;
+        var right = inRight;
+
+        while (left <= right)
+        {
+            while (list[left].CompareTo( pivot) < 0 )
+            {
+                left++;
+            }
+
+            while (list[right].CompareTo(pivot) > 0)
+            {
+                right--;
+            }
+
+            if (left <= right)
+            {
+                //сортировка на месте - просто меняем элементы местами (swap)
+                (list[left], list[right]) = (list[right], list[left]);
+
+                left++;
+                right--;
+            }
+        }
+        //рекурсивный вызов для сформированных подмассивов
+        QuickSort(ref list, inLeft, right);
+        QuickSort(ref list, left, inRight);
+    }
+
+    /// <summary>
+    /// выбор опорной точки случайным образом
+    /// </summary>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    private static int FindPivot(int left, int right)
+    {
+        Random rnd = new Random();
+        return  rnd.Next(left, right);
+    }
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var n = ReadInt();
+
+        var userInfoList = new List<UserInfo>();
+
+        for (int i = 0; i < n; i++)
+        {
+            userInfoList.Add(ReadUserInfo());
+        }
+
+        EffectiveQuickSort(userInfoList);
+
+        reader.Close();
+        writer.Close();
+    }
+    
+    private static UserInfo ReadUserInfo()
+    {
+        var userInfoArr = reader.ReadLine().Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries);
+
+        return new UserInfo(userInfoArr[0], Convert.ToInt32(userInfoArr[1]), Convert.ToInt32(userInfoArr[2]));
+
+    }
+
+    private static int ReadInt()
+    {
+        return int.Parse(reader.ReadLine());
+    }
+}
+
+public class UserInfo : IComparable<UserInfo>
+{
+    public string UserName { get; set; }
+    public int Solved { get; set; }
+    public int Penalty { get; set; }
+
+    public UserInfo(string userName, int solved, int penalty)
+    {
+        UserName = userName;
+        Solved = solved;
+        Penalty = penalty;
+    }
+
+    public override string ToString()
+    {
+        return $"{UserName}";
+    }
+
+    public int CompareTo(UserInfo? other)
+    {
+        if (this.Solved == other.Solved)
+        {
+            if (this.Penalty == other.Penalty)
+            {
+                return String.Compare(this.UserName, other.UserName);
+            }
+
+            return this.Penalty < other.Penalty ? -1 : 1;
+        }
+
+        return this.Solved < other.Solved ? 1 : -1;
+    }
+
+}
