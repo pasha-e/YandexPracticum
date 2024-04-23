d760ba2c8553c96e2c5a239e0d4ed50ee9660805
Pavel Egorov <egorov.p@gmail.com>
2024-04-23 18:07:46 +0300

    encoding fix

--- Sprint4/A.Dosug/Program.cs
+++ Sprint4/A.Dosug/Program.cs
@@ -12,34 +12,30 @@ public class A
     private static TextWriter writer;
 
     
-
     public static void Main(string[] args)
     {
-        reader = new StreamReader(Console.OpenStandardInput());
-        writer = new StreamWriter(Console.OpenStandardOutput());
-
-        //Console.OutputEncoding = Encoding.UTF8;
-        //Console.InputEncoding = Encoding.ASCII;        
+        reader = new StreamReader(Console.OpenStandardInput(),  CodePagesEncodingProvider.Instance.GetEncoding(1252));
+        writer = new StreamWriter(Console.OpenStandardOutput(), CodePagesEncodingProvider.Instance.GetEncoding(1252));
         
+        //Console.WriteLine("The encoding used was {0}.", ((StreamReader)reader).CurrentEncoding);
+        //Console.WriteLine("The encoding used was {0}.", ((StreamWriter)writer).Encoding);
+
         var n = ReadInt();
 
-        Dictionary<string, int> hashMap = new Dictionary<string, int>();
+        Dictionary<string, int> hashMap = new Dictionary<string, int>();        
 
         for (int i = 0; i < n; i++)
         {
-            var str = reader.ReadLine();
-                        
-            if(!hashMap.ContainsKey(str))
+            var str = reader.ReadLine();            
+
+            if (!hashMap.ContainsKey(str))
                 hashMap[str]  = 1;
 
             hashMap[str]++;
         }
 
         foreach (var str in hashMap.Keys)
-            writer.WriteLine(str);
-            
-
-        Console.WriteLine("привет");
+            writer.WriteLine(str);                   
 
         reader.Close();
         writer.Close();
@@ -49,24 +45,5 @@ public class A
     private static int ReadInt()
     {
         return int.Parse(reader.ReadLine());
-    }
-
-    private static string UTF8ToWin1251(string sourceStr)
-    {
-        Encoding utf8 = Encoding.UTF8;
-        Encoding win1251 = CodePagesEncodingProvider.Instance.GetEncoding(1251);
-        byte[] utf8Bytes = utf8.GetBytes(sourceStr);
-        byte[] win1251Bytes = Encoding.Convert(utf8, win1251, utf8Bytes);
-        return win1251.GetString(win1251Bytes);
-    }
-
-    static private string Win1251ToUTF8(string source)
-    {
-        Encoding utf8 = Encoding.UTF8;
-        Encoding win1251 = CodePagesEncodingProvider.Instance.GetEncoding(1251);
-        byte[] utf8Bytes = win1251.GetBytes(source);
-        byte[] win1251Bytes = Encoding.Convert(win1251, utf8, utf8Bytes);
-        source = win1251.GetString(win1251Bytes);
-        return source;
     }
 }
