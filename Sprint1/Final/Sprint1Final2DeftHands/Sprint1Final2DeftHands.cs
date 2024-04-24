d0f360cb2b8dde079999f86effe4c886826f81a2
Pavel Egorov <egorov.p@gmail.com>
2024-04-24 23:53:04 +0300

    add final tasks from sprint 1

---
+++ Sprint1/Final/Sprint1Final2DeftHands/Sprint1Final2DeftHands.cs
@@ -0,0 +1,97 @@
+﻿/*
+https://contest.yandex.ru/contest/22450/run-report/111066288/
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Sprint1Final2DeftHands
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    private const int NumberOfPlayers = 2; // число игроков
+    private const int MaxRoundNumber = 9; // максимальный номер раунда
+    private const int FieldRowCount = 4; // число строк нв поле
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="maxKey"> число клавиш которое может дажать игрок одновременно (k) </param>
+    /// <param name="fieldList">игровое поле приведенное к списку</param>
+    /// <returns></returns>    
+    private static int GetMaxResult(int maxKey, List<int> fieldList)
+    {
+        int result = 0;
+
+        int maxPlayersKey = maxKey * NumberOfPlayers;
+
+        //подготовка данных
+        List<int> numbersAtFieldList = new List<int>();
+        
+        for(int i =0; i < MaxRoundNumber; i++)
+            numbersAtFieldList.Add(0);
+
+        foreach (int cellValue in fieldList)
+            numbersAtFieldList[cellValue-1]++;
+        
+        //цикл по раундам        
+        for (int t = 1; t <= MaxRoundNumber; t++)
+        {
+            if (numbersAtFieldList[t - 1] == 0)
+                continue;
+
+            var lapNumberCount = numbersAtFieldList[t - 1];
+
+            if (lapNumberCount <= maxPlayersKey)
+                result++;
+        }
+
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var maxKeyAtMoment = ReadInt();
+
+        var fieldList = ReadFieldArr();
+        
+        var maxPoint = GetMaxResult(maxKeyAtMoment, fieldList);
+
+        writer.WriteLine(maxPoint);
+
+        reader.Close();
+        writer.Close();
+    }
+
+    /// <summary>
+    /// считываем строки, сразу отбрасываем точки и скрадываем в список. в данной задаче форма поля не имеет значения
+    /// </summary>
+    /// <returns></returns>
+    private static List<int> ReadFieldArr()
+    {
+        var result = new List<int>();
+
+        for (int i = 0; i < FieldRowCount; i++)
+        {   
+            var strRow = reader.ReadLine();
+
+            foreach(var symb in strRow)
+            {
+                if(Char.IsDigit(symb))                    
+                    result.Add(Convert.ToInt32(Char.GetNumericValue(symb)));
+            }            
+        }
+
+        return result;
+    }
+
+    private static int ReadInt()
+    {
+        return int.Parse(reader.ReadLine());
+    }
+}
---
+++ Sprint1/Final/Sprint1FinalNearZero/Sprint1Final1NearestZero.cs
@@ -0,0 +1,100 @@
+﻿/*
+ https://contest.yandex.ru/contest/22450/run-report/110979297/
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class Sprint1Final1NearestZero
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    /// <summary>
+    /// получить список расстояний до ближайшего пустого участка
+    /// (по уму длинна улицы нам тут в целом не нужна, можно получать её как размер списка
+    /// но раз в задании она есть и отдельно считывается, будем её использовать
+    /// будем считать фукцию более универсальной, как будто она может анализировать скажем не весь список)
+    /// </summary>
+    /// <param name="numberList">список участков на улице</param>
+    /// <param name="lenght">число участов на улице</param>
+    /// <returns></returns>
+    private static List<int> GetNearestZeroPosition(List<int> numberList, int lenght)
+    {
+        List<int> zeroPosition = new List<int>();
+
+        List<int> result = new List<int>();
+
+        //получим список позиций пустых участков
+        for(int i=0; i < lenght; i++)
+        {
+            if (numberList[i] == 0) 
+                zeroPosition.Add(i);
+        }
+
+        for (int i = 0; i < lenght; i++)
+        {
+            if (numberList[i] == 0)
+            {
+                result.Add(0);
+                continue;
+            }
+
+            result.Add(GetMinNearestValue(zeroPosition, i));
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// получить миинмальное расстояние от текущего участка до ближайшего пустого
+    /// (от текущего индекса до ближайшего нулевого индекса)
+    /// </summary>
+    /// <param name="zeroPositonList"></param>
+    /// <param name="position"></param>
+    /// <returns></returns>
+    private static int GetMinNearestValue(List<int> zeroPositonList, int position)
+    {
+        int result = Math.Abs(position - zeroPositonList[0]);
+
+        for (int i = 1; i < zeroPositonList.Count; i++)
+        {
+            var distance = Math.Abs(position - zeroPositonList[i]);
+            
+            if (distance < result)
+                result = distance;
+        }
+       
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var streetLength = ReadInt();
+        var numberList = ReadList();        
+
+        var sum = GetNearestZeroPosition(numberList, streetLength);
+
+        writer.WriteLine(string.Join(" ", sum));
+
+        reader.Close();
+        writer.Close();
+    }
+
+    private static List<int> ReadList()
+    {
+        return reader.ReadLine()
+            .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+    }
+
+    private static int ReadInt()
+    {
+        return int.Parse(reader.ReadLine());
+    }
+}
