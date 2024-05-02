54e78222a7d9b21b7324ca22e4599075bd0e914d
Pavel Egorov <egorov.p@gmail.com>
2024-05-02 23:52:37 +0300

    add final task 1 from sprint 4

--- Sprint4/Final/Final1.SearchSystem/Program.cs
+++ Sprint4/Final/Final1.SearchSystem/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿/*
+ https://contest.yandex.ru/contest/24414/run-report/113401454/
+ */
+/*
+ -- ПРИНЦИП РАБОТЫ --
+   
+
+   
+   -- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+   
+       
+   
+   -- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+   
+       
+   -- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+   
+   n - число документов  по макс. 1000 символов
+    макс. n - 10^4
+
+    на построение индекса нужно O(n) доп. памяти
+    
+      
+ */
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Enumeration;
 using System.Linq;
-using System.Reflection.Metadata;
 
-public class SearchSystem
+public class Final1SearchSystem
 {
+    private const int OutLimit = 5;
+    private const int MacDocsCount = 10*10*10*10;
+
     private static TextReader reader;
     private static TextWriter writer;
 
@@ -27,35 +53,77 @@ public class SearchSystem
         
         var m = ReadInt();
 
-        List<List<string>> requestsData = new List<List<string>>();
+        List<HashSet<string>> requestsData = new List<HashSet<string>>();
+        //List<List<string>> requestsData = new List<List<string>>();
 
         for (int i = 0; i < m; i++)
         {
-            requestsData.Add(ReadStrList());
+            /*
+             var strList = ReadStrList();
+            strList = strList.Distinct().ToList();
+
+            requestsData.Add(strList);
+            */
+            requestsData.Add(ReadHashSet());
         }
         
         var searchIndex = CreateSearchIndex(inputData);
 
+        //по каждой строчке Запроса
         foreach (var requestStr in requestsData)
         {
+            //считаем релевантность
             var resultOneDoc = FindRelevantDocuments(searchIndex, requestStr, limit: 5, numDocs: n+1);
 
-            foreach (var res in resultOneDoc)
-            {
-                writer.Write($"{res} ");
-            }
-            writer.WriteLine();
+            PrintLine(resultOneDoc);
         }
 
         reader.Close();
         writer.Close();
     }
 
-    private static List<int> FindRelevantDocuments(Dictionary<string, Dictionary<int, int>> searchIndex, List<string> requestsData, int limit = 5, int numDocs = 10000)
+    
+    private static Dictionary<string, Dictionary<int, int>> CreateSearchIndex(List<List<string>> documents)
     {
-        var res = new List<List<int>>();
+        //word : (document : weight)
+        Dictionary<string, Dictionary<int, int>> searchIndexDictionary = new Dictionary<string, Dictionary<int, int>>();
+
+        //по всем документам
+        for (int i = 0; i < documents.Count; i++)
+        {
+            //переложим список в словарь word : weight
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (var word in documents[i])
+            {
+                if (!dict.ContainsKey(word))
+                {
+                    dict.Add(word, 1);
+                    continue;
+                }
+
+                dict[word]++;
+            }
+
+            foreach (var pair in dict)
+            {
+                if (!searchIndexDictionary.ContainsKey(pair.Key))
+                {
+                    searchIndexDictionary.Add(pair.Key, new Dictionary<int, int>());
+                }
+
+                searchIndexDictionary[pair.Key][i+1] = pair.Value;
+            }
+        }
+
+        return searchIndexDictionary;
+    }
+
+    private static List<int> FindRelevantDocuments(Dictionary<string, Dictionary<int, int>> searchIndex, HashSet<string> requestsData, int limit = OutLimit, int numDocs = MacDocsCount)
+    {
+        var res = new List<int[]>();
+
         for(int i=0; i < numDocs; i++)
-            res.Add(new List<int>(){0, i});
+            res.Add(new int[] { 0, i });
 
         foreach (var word in requestsData)
         {
@@ -72,6 +140,7 @@ public class SearchSystem
         //отсортируем по весу
         res.Sort(CompareByWeight);
 
+        //подготовка вывода (проверрка на лимит и !0)
         List<int> resultIndexes = new List<int>();
 
         for (int i = 0; i < res.Count; i++)
@@ -86,12 +155,15 @@ public class SearchSystem
         return resultIndexes;
     }
 
-    private static int CompareByWeight(List<int> x, List<int> y)
+    private static int CompareByWeight(int[] x, int[] y)
     {
         if (x == null && y == null) return 0;
 
         else if (x == null) return -1;
         else if (y == null) return 1;
+
+        else if (x[0] == y[0])
+            return x[1].CompareTo(y[1]);
 
         else return x[0].CompareTo(y[0]);
     }
@@ -108,47 +180,32 @@ public class SearchSystem
             .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries).ToList();
     }
 
-
-    private static Dictionary<string, Dictionary<int, int>> CreateSearchIndex(List<List<string>> documents)
+    private static HashSet<string> ReadHashSet()
     {
-        Dictionary<string, Dictionary<int, int>> searchIndexDictionary = new Dictionary<string, Dictionary<int, int>>();
+        var hashSet = new HashSet<string>();
 
-        for (int i = 0; i < documents.Count; i++)
+        var strList = ReadStrList();
+        foreach (var str in strList)
         {
-            //переложим список в словарь
-            Dictionary<string, int> dict = new Dictionary<string, int>();
-            foreach (var word in documents[i])
-            {
-                if (!dict.ContainsKey(word))
-                {
-                    dict.Add(word, 1);
-                    continue;
-                }
-
-                dict[word]++;
-            }
-
-            foreach (var pair in dict)
-            {
-                //searchIndexDictionary[(pair.Key, i)] = pair.Value;
-
-                if (!searchIndexDictionary.ContainsKey(pair.Key))
-                {
-                    searchIndexDictionary.Add(pair.Key, new Dictionary<int, int>());
-                }
-
-                searchIndexDictionary[pair.Key][i+1] = pair.Value ;
-
-            }
-
+            hashSet.Add(str);
         }
 
-        return searchIndexDictionary;
+        return hashSet;
+    }
+
+    private static void PrintLine(List<int> list)
+    {
+        foreach (var val in list)
+        {
+            writer.Write($"{val} ");
+        }
+        writer.WriteLine();
     }
 }
 
 
 /*
+
 6
 buy flat in moscow
 rent flat in moscow
@@ -159,7 +216,9 @@ renovate flat in moscow
 1
 flat in moscow for crazy weekends
    
+4 5 1 2 3
 
+--------------------------------------------
 
 3
 i like dfs and bfs
@@ -169,4 +228,21 @@ i like bfs with bfs and bfs
 dfs dfs dfs dfs bfs
 
 3 1 2
+
+--------------------------------------------
+
+3
+i love coffee
+coffee with milk and sugar
+free tea for everyone
+3
+i like black coffee without milk
+everyone loves new year
+mary likes black coffee without milk
+
+1 2
+3
+2 1
+
+
  */
