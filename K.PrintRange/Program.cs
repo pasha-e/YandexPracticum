2fe47d0e399d837b403cc0c779f1939e90933348
Pavel Egorov <egorov.p@gmail.com>
2024-05-24 18:23:27 +0300

    add task K

---
+++ K.PrintRange/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+#if !REMOTE_JUDGE
+public class Node
+{
+    public int Value { get; set; }
+    public Node Left { get; set; }
+    public Node Right { get; set; }
+
+    public Node(int value)
+    {
+        Value = value;
+        Left = null;
+        Right = null;
+    }
+}
+#endif
+
+public class Solution
+{
+    public static void PrintRange(Node root, int left, int right, StreamWriter writer)
+    {
+        if(root == null) 
+            return;
+
+        if (root.Value >= left)
+        {
+            PrintRange(root.Left, left, right, writer);
+        }
+
+        if (left <= root.Value && root.Value <= right)
+        {
+            writer.WriteLine(root.Value);
+        }
+
+        if (root.Value <= right)
+        {
+            PrintRange(root.Right, left, right, writer);
+        }
+
+    }
+}
