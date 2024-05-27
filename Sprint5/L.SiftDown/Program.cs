19a6ca85f1780fd95ca60555f441d6ea2d5d1161
Pavel Egorov <egorov.p@gmail.com>
2024-05-27 17:15:18 +0300

    add tasks L . M

---
+++ Sprint5/L.SiftDown/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class Solution
+{
+    public static int SiftDown(List<int> array, int idx)
+    {
+        int left = 2 * idx;
+        int right = 2 * idx + 1;
+
+        // Нет дочерних узлов
+        if (left >= array.Count)
+        {
+            return idx;
+        }
+
+        // right < heap.Count проверяет, что есть оба дочерних узла
+        int indexLargest = (right < array.Count && array[right] > array[left]) ? right : left;
+
+        if (array[idx] >= array[indexLargest])
+            return idx;
+
+        (array[idx], array[indexLargest]) = (array[indexLargest], array[idx]);
+
+        return SiftDown(array, indexLargest);
+
+        /*
+        if (array[indexLargest] > array[idx])
+        {
+            int temp = array[idx];
+            array[idx] = array[indexLargest];
+            array[indexLargest] = temp;
+            SiftDown(array, indexLargest);
+        }
+
+        return indexLargest;*/
+    }
+
+#if !REMOTE_JUDGE
+    private static void Main()
+    {
+        var sample = new List<int> { -1, 12, 1, 8, 3, 4, 7 };
+        //System.Console.WriteLine(SiftDown(sample, 2));
+        System.Console.WriteLine(SiftDown(sample, 2) == 5);
+    }
+#endif
+}
---
+++ Sprint5/M.SiftUp/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public static int SiftUp(List<int> array, int idx)
+    {
+        if (idx == 1)
+        {
+            return idx;
+        }
+
+        int parentIndex = idx / 2;
+
+        if (array[parentIndex] >= array[idx])
+            return idx;
+
+
+        (array[parentIndex], array[idx]) = (array[idx], array[parentIndex]);
+
+        return SiftUp(array, parentIndex);
+    }
+
+#if !REMOTE_JUDGE
+    private static void Main()
+    {
+        var sample = new List<int> { -1, 12, 6, 8, 3, 15, 7 };
+        System.Console.WriteLine(SiftUp(sample, 5) == 1);
+    }
+#endif
+}
