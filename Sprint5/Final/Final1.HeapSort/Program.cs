d9df15b9a7e4946a9a88cb7cef78de196c2e3d71
Pavel Egorov <egorov.p@gmail.com>
2024-05-28 15:21:30 +0300

    add samples to  final task in sprint 5

--- Sprint5/Final/Final1.HeapSort/Program.cs
+++ Sprint5/Final/Final1.HeapSort/Program.cs
@@ -228,3 +228,48 @@ public class UserInfo : IComparable<UserInfo>
     }
 
 }
+
+
+/*
+ 5
+alla 6 100
+gena 10 1000
+gosha 2 90
+rita 8 90
+timofey 4 80
+
+gena
+rita
+alla
+timofey
+gosha
+
+
+5
+alla 4 100
+gena 6 1000
+gosha 2 90
+rita 2 90
+timofey 4 80
+
+gena
+timofey
+alla
+gosha
+rita
+
+
+5
+alla 0 0
+gena 0 0
+gosha 0 0
+rita 0 0
+timofey 0 0
+
+alla
+gena
+gosha
+rita
+timofey
+
+ */
