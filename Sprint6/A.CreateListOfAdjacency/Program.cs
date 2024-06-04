5c4ec628eab7d428021b9ac455375fcb89a5521a
Pavel Egorov <egorov.p@gmail.com>
2024-06-04 14:14:11 +0300

    Add tasks A,B to Sprint 6

---
+++ Sprint6/A.CreateListOfAdjacency/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class A
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    private static void CreateAdjacencyList(int n, List<(int, int)> edgesList)
+    {
+        Dictionary<int, List<int>> map = new Dictionary<int, List<int>>();
+
+        for(int i = 1; i <= n; i++)
+            map.Add(i, new List<int>());
+
+
+        foreach (var edge in edgesList)
+        {
+            map[edge.Item1].Add(edge.Item2);
+        }
+
+        foreach (var pair in map)
+        {
+            pair.Value.Sort();
+        }
+
+        foreach (var pair in map)
+        {
+            var count = pair.Value.Count;
+
+            string result = $"{count}";
+
+            if (count > 0)
+            {
+                foreach (var value in pair.Value)
+                {
+                    result += $" {value}";
+                }
+            }
+
+            writer.WriteLine(result);
+
+        }
+    }
+
+    private static void CreateAdjacencyList2(int n, List<(int, int)> edgesList)
+    {
+        List<List<int>> list = new ();
+
+        for (int i = 0; i < n; i++)
+            list.Add( new List<int>());
+
+
+        foreach (var edge in edgesList)
+        {
+            list[edge.Item1-1].Add(edge.Item2);
+        }
+
+        foreach (var data in list)
+        {
+            data.Sort();
+        }
+
+        foreach (var data in list)
+        {
+            var count = data.Count;
+
+            string result = $"{count}";
+
+            if (count > 0)
+            {
+                foreach (var value in data)
+                {
+                    result += $" {value}";
+                }
+            }
+
+            writer.WriteLine(result);
+
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var nm = ReadList();
+
+
+        var edges  = new List<(int,int)> ();
+
+        for (int i = 0; i < nm[1]; i++)
+        {
+            var uv = ReadList();
+
+            edges.Add((uv[0], uv[1]));
+        }
+
+        CreateAdjacencyList(nm[0], edges);
+
+        reader.Close();
+        writer.Close();
+    }
+
+
+    private static List<int> ReadList()
+    {
+        return reader.ReadLine()
+            .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+    }
+}
+
+/*
+
+5 3
+1 3
+2 3
+5 2
+
+
+1 3 
+1 3 
+0 
+0 
+1 2 
+ */
---
+++ Sprint6/B.CreateMatrixOfAdjacency/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class B
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    private static void CreateAdjacencyMatrix(int n, List<(int, int)> edgesList)
+    {
+        int[,] matrix = new int[n,n];
+
+        for(int i=0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                matrix[i,j] = 0;
+
+
+        foreach (var edge in edgesList)
+        {
+            matrix[edge.Item1-1, edge.Item2-1] = 1;
+        }
+
+
+        for (int i = 0; i < n; i++)
+        {
+            string line = String.Empty;
+
+            for (int j = 0; j < n; j++)
+            {
+                line += $"{matrix[i,j]} ";
+            }
+
+            writer.WriteLine(line);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var nm = ReadList();
+
+
+        var edges = new List<(int, int)>();
+
+        for (int i = 0; i < nm[1]; i++)
+        {
+            var uv = ReadList();
+
+            edges.Add((uv[0], uv[1]));
+        }
+
+        CreateAdjacencyMatrix(nm[0], edges);
+
+        reader.Close();
+        writer.Close();
+    }
+
+
+    private static List<int> ReadList()
+    {
+        return reader.ReadLine()
+            .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+    }
+}
+
+/*
+
+5 3
+1 3
+2 3
+5 2
+
+
+0 0 1 0 0 
+0 0 1 0 0 
+0 0 0 0 0 
+0 0 0 0 0 
+0 1 0 0 0 
+ */
