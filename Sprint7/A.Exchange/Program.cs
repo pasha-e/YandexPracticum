628c07146c6ec06e8865a31b121fad59cb296ada
Pavel Egorov <egorov.p@gmail.com>
2024-06-13 18:13:48 +0300

    add task A in sprint 7

---
+++ Sprint7/A.Exchange/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.IO;
+using System.Linq;
+
+public class A
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        var n = ReadInt();
+
+        var prices = ReadList();
+
+        var profit = CalcBestPrice(prices);
+
+        writer.WriteLine(profit);
+        
+        reader.Close();
+        writer.Close();
+    }
+
+    private static int CalcBestPrice(List<int> prices)
+    {
+        var bestProfit = 0;
+
+        for (int i = 0; i < prices.Count-1; i++)
+        {
+            if (prices[i] < prices[i + 1])
+                bestProfit += prices[i+1] - prices[i];
+        }
+
+        return bestProfit;
+    }
+
+
+    private static List<int> ReadList()
+    {
+        return reader.ReadLine()
+            .Split(new[] { ' ', '\t', }, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+    }
+
+    private static int ReadInt()
+    {
+        return int.Parse(reader.ReadLine());
+    }
+}
+
+/*
+
+
+ */
