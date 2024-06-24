dd42aae99df3ec9d615fdc8bbb97299a50f8ec4d
Pavel Egorov <egorov.p@gmail.com>
2024-06-24 17:34:25 +0300

    add final task 1 in sprint 7

---
+++ Sprint7/Final/A.DistanceByLivenshtain/Program.cs
@@ -0,0 +1,134 @@
+﻿
+/*
+
+ */
+/*
+     -- ПРИНЦИП РАБОТЫ --
+
+    https://ru.wikipedia.org/wiki/%D0%A0%D0%B0%D1%81%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D0%B5_%D0%9B%D0%B5%D0%B2%D0%B5%D0%BD%D1%88%D1%82%D0%B5%D0%B9%D0%BD%D0%B0
+    https://habr.com/ru/articles/676858/
+     
+     -- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+   
+    
+        
+     -- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+    
+   
+    
+   */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class DistByLivenstain
+{
+    private static TextReader reader;
+    private static TextWriter writer;
+
+    public static void Main(string[] args)
+    {
+        reader = new StreamReader(Console.OpenStandardInput());
+        writer = new StreamWriter(Console.OpenStandardOutput());
+
+        string str1 = reader.ReadLine();
+        string str2 = reader.ReadLine();
+
+        int dist = CalcDistByLivestain(str1, str2);
+
+        writer.WriteLine(dist);
+
+        reader.Close();
+        writer.Close();
+    }
+
+    private static int CalcDistByLivestain(string str1, string str2)
+    {
+        var n = str1.Length;
+        var m = str2.Length;
+
+        if (n > m)
+        {
+            (str1, str2) = (str2, str1);
+            (n, m) = (m, n);
+        }
+
+        int[] currentRow = new int[n + 1];
+        for(int i=0; i<currentRow.Length; i++)
+            currentRow[i] = i;
+
+        int[] prevRow = new int[n + 1];
+
+        for (int i = 1; i <= m; i++)
+        {
+            for (int k = 0; k < prevRow.Length; k++)
+                prevRow[k] = 0;
+
+            prevRow[0] = i;
+
+            (prevRow, currentRow) = (currentRow, prevRow);
+
+
+            for (int j = 1; j <= n; j++)
+            {
+                var add = prevRow[j] + 1;
+                var delete = currentRow[j-1] + 1;
+                var change = prevRow[j-1];
+
+                if (str1[j - 1] != str2[i - 1])
+                    change += 1;
+
+                currentRow[j] = Min(add,delete,change);
+            }
+
+        }
+
+        return currentRow[n];
+    }
+
+    private static int Min(int a, int b, int c)
+    {
+        int[] arr = new[] { a, b, c };
+
+        return arr.Min();
+    }
+}
+
+/*
+abacaba
+abaabc
+
+2
+
+innokentiy
+innnokkentia
+
+3
+
+r
+x
+
+1
+
+dxqrpmratn
+jdpmykgmaitn
+
+8
+
+yfalbhmqrz
+twaqbre
+
+7
+
+pwhsvrojcj
+uepwplklojcj
+
+6
+
+gthryaownz
+ouuxcwysek
+
+10
+
+ */
